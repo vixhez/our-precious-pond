72c5ae67b155642843bea60051cf8387e72cb77e
vixhez <vicky_herod@live.com>
2022-05-14 19:23:41 +0100

    feat / database entries now rendering within react app

---
+++ quack-frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import RecordList from './recordList.js';
+
+function App() {
+  return (
+    <div className="App">
+      <p>hello? anyone there?</p>
+      <RecordList />  
+    </div>
+  );
+}
+
+export default App;
