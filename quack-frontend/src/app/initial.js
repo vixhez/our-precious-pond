6120e5e81e979dd1b7dbf6673e21098199c9046e
vixhez <vicky_herod@live.com>
2022-07-20 23:40:27 +0100

    feat / creation of initial.js file in order to utilise state on submitting of quiz

---
+++ quack-frontend/src/app/initial.js
@@ -0,0 +1,29 @@
+const initial = {
+    ducks: [],
+    ducksLoaded: false,
+
+    // traitInputs: {
+    //     extroversion1: '',
+    //     extroversion2: '',
+    //     colourfulness1: '',
+    //     colourfulness2: '',
+    //     generosity1: '',
+    //     generosity2: '',
+    //     activeness1: '',
+    //     activeness2: ''
+    // },
+
+    extroversionScore: 5,
+    colourfulnessScore: 5,
+    generosityScore: 5,
+    activenessScore: 5,
+
+    quizComplete: false,
+
+    duckAlterEgo: {},
+    frogAlterEgo: {},
+
+    showAllDucks: false
+}
+
+export default initial;
--- quack-frontend/src/components/Quiz/Quiz.js
+++ quack-frontend/src/components/Quiz/Quiz.js
@@ -4,10 +4,21 @@ import React, { useEffect, useState } from "react";
 
 export default function DisplayQuiz() {
 	const [quizContent, setQuizContent] = useState([]);
+    const [scores, setScores] = useState({
+        activeness_0: 0,
+        activeness_1: 0,
+        vibrance_0: 0,
+        vibrance_1: 0,
+        generosity_0: 0,
+        generosity_1: 0,
+        extroversion_0: 0,
+        extroversion_1: 0
+    });
 	
 	// This method fetches the records from the database.
 	useEffect(() => {
 		async function getQuestions() {
+            console.log('get questions');
 			let response = await fetch(`http://localhost:5000/duck_quiz`);
 	
 			if (!response.ok) {
@@ -26,28 +37,29 @@ export default function DisplayQuiz() {
 
 
 
-
     function renderQuiz() {
+
+        console.log('render quiz');
         let quiz = [];
 
         quizContent.map(quizCategory => {
             let quizItems = Object.values(quizCategory)[1];
-            quizItems.map(quizItem => {
+            quizItems.map((quizItem, index) => {
                 let answersQuantity = Object.keys(quizItem).filter(key => key.includes('answer')).length;
 
                 let quizItemAnswers = [];
                 for (let i = 0; i < answersQuantity; i++) {
                     let currentAnswer = `answer_${i+1}`;
                     quizItemAnswers.push(
-                        <div className={`quiz-answer_${i}`}>
+                        <div className={`quiz__answer`}>
                             <input
-                                onChange={() => console.log('0101010')}
+                                onClick={handleInputClick}
                                 type="radio"
-                                id={`${Object.keys(quizCategory)[1]}-answer-${i}`}
-                                name={`${Object.keys(quizCategory)[1]}-answer-${i}`}
-                                value="high"
+                                id={`${Object.keys(quizCategory)[1]}-answer-${index}-${i}`}
+                                name={`${Object.keys(quizCategory)[1]}_${index}`}
+                                value={`${Object.keys(quizCategory)[1]}-${i}`}
                             />
-                            <label htmlFor={`${Object.keys(quizCategory)[1]}-answer-${i}`}>
+                            <label htmlFor={`${Object.keys(quizCategory)[1]}-answer-${index}`}>
                                 {quizItem[currentAnswer]}
                             </label>
                         </div>
@@ -60,23 +72,47 @@ export default function DisplayQuiz() {
                         <div className="quiz__question">
                             {quizItem.question}
                         </div>
-                            <div className="quiz__answers">
+                            <div className="quiz__answers-container">
                                 {quizItemAnswers}
                             </div>
                         </div>
 
                 </>
                 );
-
             })
         })
 
         return quiz;
     }
 
+    function handleInputClick(event) {
+
+
+
+        console.log('scores', scores);
+
+        console.log('hello, inside handle input click', event.target.value);
+        let answerValue = parseInt(event.target.value.split('-')[1]);
+
+        setScores({...scores, [event.target.name]: answerValue});
+
+        console.log('scores onceth againeth', scores);
+
+
+        console.log('answer value', answerValue);
+    }
+
     return (
         <div className="quiz">
-            {renderQuiz()};
+            {renderQuiz()}
         </div>
     )
 }
+
+// https://stackoverflow.com/questions/46138145/where-should-functions-in-function-components-go refer to this for structure
+
+// look into how to stop re-rendering on load https://linguinecode.com/post/how-to-avoid-multiple-re-renders-in-react-shouldcomponentupdate 
+
+//https://medium.com/@katestamas/dynamically-update-states-in-react-7558287e5fb9
+
+// https://stackoverflow.com/questions/29280445/reactjs-setstate-with-a-dynamic-key-name
