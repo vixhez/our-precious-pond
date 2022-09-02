16955c965f178c3010f07df2a4092ff74d56d56d
vixhez <vicky_herod@live.com>
2022-09-02 23:13:10 +0100

    styling / header styled for different screen widths

--- quack-frontend/src/components/Header.js
+++ quack-frontend/src/components/Header.js
@@ -2,9 +2,10 @@ import Mallard from '../assets/ducks/general-imagery/mallard_transparent.png';
 
 export default function Header() {
     return (
-        <>
+        <div class="app-header">
             <img src={Mallard} alt="" />
             <h1>Our Precious Pond</h1>
-        </>
+            <span>✧</span>
+        </div>
     )
 }
